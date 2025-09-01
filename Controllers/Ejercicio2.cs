268269f80eb19d4f7452cbd4f835dfa1a1fb7f88
GhostGold2025 <Santoniogonzalezflores@yahoo.es>
2025-09-01 00:09:23 -0600

    Subiendo mi carpeta al repositorio

---
+++ Controllers/Ejercicio2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea1.Controllers
+{
+	public class Ejercicio2 : Controller
+	{
+		private List<string> nombres = new List<string>
+		{
+			"Crisanta", "Luisardo", "Esdras", "Isidora", "Bernadetta", "Arno", "John", "Galo", "Sofia", "Volnutt"
+		};
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string BuscarPosicion(int posicion)
+		{
+			if (posicion < 0 || posicion >= nombres.Count)
+			{
+				return "Posición inválida";
+			}
+
+			return nombres[posicion];
+		}
+
+		public int BuscarCadena(string nombre)
+		{
+			return nombres.FindIndex(n => n.Equals(nombre));
+		}
+	}
+}
